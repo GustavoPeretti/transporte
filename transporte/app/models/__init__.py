4d00c14cceccd778b35e6aef2de7fd3949be81a3
João Vitor Raimundi <joaoraimundi@gmail.com>
2026-06-02 20:28:50 -0300

    criação model perfil_usuario

--- transporte/app/models/__init__.py
+++ transporte/app/models/__init__.py
@@ -1,3 +1,5 @@
 from .veiculo import Veiculo
 from .planejamento import Planejamento
 from .usuario import Usuario
+from .instituicao import Instituicao
+from .perfil_passageiro import PerfilPassageiro
