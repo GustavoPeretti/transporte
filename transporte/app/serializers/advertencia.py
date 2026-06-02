3095165505ef55ee232c699d7589415c36d90d58
GustavoPeretti <gustavoperetti11@gmail.com>
2026-06-02 20:37:46 -0300

    Implementação de serializers

---
+++ transporte/app/serializers/advertencia.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from ..models.advertencia import Advertencia
+
+class AdvertenciaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Advertencia
+        fields = ["id", "confirmacao", "descricao", "data"]
